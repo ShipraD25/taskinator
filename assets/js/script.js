55335f63214d37462f8c3401363cee64f2b0c49d
Shipra <drshipradyundi@gmail.com>
2020-08-25 22:22:57 -0700

    added code to capture form content and add it to our task list

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,21 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function () {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     
     var lisItemE1 =document.createElement("li");
     lisItemE1.className ="task-item";
-    lisItemE1.textContent = "This is a new task. ";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    lisItemE1.appendChild(taskInfoEl);
+    // add entire list item to list
     tasksToDoE1.appendChild(lisItemE1);
  };
 
